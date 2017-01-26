b94a51b9e75b8bb50fee99978827ce48689237dc
Gabriel Revells <gcrevell@mtu.edu>
2017-01-25 20:41:44 -0500

    Initial Commit

---
+++ NotifyMe.Cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NotifyMe.Cpp
+//
+//  Created by Voltmeter Amperage on 1/25/17.
+//  Copyright © 2017 ReVoltApplications. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
